33557eb8c802afdebe89e72ce9275622d868bb50
Rohit Gottam <rgottam@cisco.com>
2018-03-20 11:23:24 -0700

    Base changes with react-router.

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> About </h1>
+        <p>This app uses akjd flaskdfj asldjf ajdf aldfldsjf adf</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
